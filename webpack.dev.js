38de0f2b99629621e1f54e4c4aa1bea5d8e5b98c
abdo2017 <abdoesam14@gmail.com>
2022-01-03 14:35:27 +0200

    mod

--- webpack.dev.js
+++ webpack.dev.js
@@ -32,11 +32,8 @@ module.exports = {
             filename: "./index.html",
         }),
         new CleanWebpackPlugin({
-            // Simulate the removal of files
             dry: true,
-            // Write Logs to Console
             verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
